9a9702aa7e1ce412e212ba8936f856a60c92208a
srest2021 <sofia.rest@sentry.io>
2025-07-24 14:55:13 -0700

    add traces

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://7ba3dd399bcd09c145c91ac1ce022568@o4509714332450816.ingest.us.sentry.io/4509725614735360",
+  
+  release: "1.0.0",
+  
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
+  
   integrations: [
     Sentry.replayIntegration(),
+    Sentry.browserTracingIntegration(),
     Sentry.feedbackIntegration({
       // The SDK will automatically create a button and inject it into the DOM
       // You can also customize the button appearance
@@ -29,9 +34,17 @@ Sentry.init({
       ],
     }),
   ],
+
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost" ],
 });
 
 function App() {
@@ -49,7 +62,7 @@ function App() {
         >
           Learn React
         </a>
-        <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>;
+        <button onClick={() => {throw new Error("What's happening!");}}>Click me</button>;
       </header>
     </div>
   );
