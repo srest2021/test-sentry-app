623bb26838ff48f770350eebe11af9b4ea676ace
srest2021 <sofia.rest@sentry.io>
2025-07-25 09:37:37 -0700

    fix release

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,11 @@ import * as Sentry from "@sentry/react";
 Sentry.init({
   dsn: "https://7ba3dd399bcd09c145c91ac1ce022568@o4509714332450816.ingest.us.sentry.io/4509725614735360",
   
-  release: "1.0.0",
+  release: `test-app@1.0.0`,
   
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
   sendDefaultPii: true,
-  
   integrations: [
     Sentry.replayIntegration(),
     Sentry.browserTracingIntegration(),
@@ -34,7 +33,6 @@ Sentry.init({
       ],
     }),
   ],
-
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
